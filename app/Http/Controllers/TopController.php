5fc0c9b8964a9d59323e0cf0c6dc477b4000e753
“Fumiteru <runprotsuru@gmail.com>
2020-06-18 14:33:35 +0900

    TopControllerの不要箇所削除

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -4,29 +4,25 @@ namespace App\Http\Controllers;
 
 use App\circle;
 use App\Point;
-use App\Genre;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class TopController extends Controller
 {
- public function index(Request $request) {
-   $user = Auth::user();
-   $points = Point::all();
-   $items = circle::all();
-//    echo "<pre>";
-//    var_dump($items);
-//    echo "</pre>";
-//    exit();
-   return view('top.index',['points' => $points, 'items' => $items,'user' => $user]);
-  }
-  public function index_post(Request $request) {
-     $items  = circle::all();
+
+  public function index(Request $request) {
+     $user = Auth::user();
      $points = Point::all();
-     //    echo "<pre>";
-     //    var_dump($points);
-     //    echo "</pre>";
-     //    exit();
-     return view('top.index',['items' => $items, 'points' => $points]);
+     $items = circle::all();
+
+     return view('top.index',['user' => $user, 'points' => $points, 'items' => $items]);
   }
+
+  public function index_post(Request $request) {
+     $points = Point::all();
+     $items  = circle::all();
+
+     return view('top.index',['points' => $points, 'items' => $items]);
+  }
+
 }
